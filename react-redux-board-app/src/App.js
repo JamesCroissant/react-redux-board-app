c7e4f7ae143c39f7a431856b6f2a68297925fed7
Yu Hamada <yuham1014@gmail.com>
2023-10-10 13:35:24 -0700

    update App.js

--- react-redux-board-app/src/App.js
+++ react-redux-board-app/src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addPost } from './redux/postSlice';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
   const postList = useSelector((state) => state.posts.value);
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(addPost({
+      id: uuidv4(),
+      name: name,
+      content: content
+    }));
+
+    setName("");
+    setContent("");
+  };
 
   return (
     <div className="App">
@@ -10,9 +30,9 @@ function App() {
         <h1>react-redux-board</h1>
       </div>
       <div className="addPost">
-        <input type="text" placeholder="name" />
-        <input type="text" placeholder="content" />
-        <button>post</button>
+        <input type="text" placeholder="name" onChange={(e) => setName(e.target.value)} value={name} />
+        <input type="text" placeholder="content" onChange={(e) => setContent(e.target.value)} value={content} />
+        <button onClick={() => handleClick()}>post</button>
       </div>
       <div className="displayPosts">
         {postList.map((post) => (
