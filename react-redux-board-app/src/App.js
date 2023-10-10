c608174d73a142314e5ba3f24829dcee80c83b42
Yu Hamada <yuham1014@gmail.com>
2023-10-10 13:18:52 -0700

    update App.js

--- react-redux-board-app/src/App.js
+++ react-redux-board-app/src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
+import { useSelector } from "react-redux";
 
 function App() {
+  const postList = useSelector((state) => state.posts.value);
+
   return (
     <div className="App">
+      <div>
+        <h1>react-redux-board</h1>
+      </div>
+      <div className="addPost">
+        <input type="text" placeholder="name" />
+        <input type="text" placeholder="content" />
+        <button>post</button>
+      </div>
+      <div className="displayPosts">
+        {postList.map((post) => (
+          <div key={post.id} className="post">
+            <h2>{post.name}</h2>
+            <h2 className="postContent">{post.content}</h2>
+            <button>delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
